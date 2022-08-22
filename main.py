3146345e88cb62e6a59de3a62521e74c6a8a5843
Justinalan0965 <mraj56965@gmail.com>
2022-08-22 21:11:18 +0530

    again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 
-API_KEY = 5729641344:AAHo-ghSD97FFEEu0W1GeWVjuWvvxRtMphw
+API_KEY = "5729641344:AAHo-ghSD97FFEEu0W1GeWVjuWvvxRtMphw"
 bot = telebot.TeleBot(API_KEY)
 @bot.message_handler(commands=["Hi"])
 def hi(message):
