5e0cd3e50d0b6f2e048760153e247cd109c729ce
Justinalan0965 <mraj56965@gmail.com>
2022-08-22 21:25:07 +0530

    gayboi

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ API_KEY = "5729641344:AAHo-ghSD97FFEEu0W1GeWVjuWvvxRtMphw"
 bot = telebot.TeleBot(API_KEY)
 @bot.message_handler(commands=["Hi"])
 def hi(message):
-    bot.reply_to(message, "Suck my Chicken!")
+    bot.reply_to(message, "pooarasu loves priyanka mohan")
 
 bot.polling()
