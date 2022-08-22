354b08f1b8ce0016d5b69fed792b1479d3c60208
Justinalan0965 <mraj56965@gmail.com>
2022-08-22 21:05:17 +0530

    correction

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from imaplib import Commands
 import telebot
 
 API_KEY = 5729641344:AAHo-ghSD97FFEEu0W1GeWVjuWvvxRtMphw
 bot = telebot.TeleBot(API_KEY)
-@bot.message_handler(Commands=["Hi"])
+@bot.message_handler(commands=["Hi"])
 def hi(message):
     bot.reply_to(message, "Suck my Chicken!")
 
